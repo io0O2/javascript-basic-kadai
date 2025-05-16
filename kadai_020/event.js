15f4174569f2afd8b8a40c56f9a280c19af53f8c
io0O2 <transparence.11@outlook.com>
2025-05-16 13:08:07 +0900

    kadai_020を再アップロード

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,8 @@
 
 const btn = document.getElementById('btn');
+const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-    const text = document.getElementById('text');
-    text.textContent = 'ボタンをクリックしました';
+        text.textContent = 'ボタンをクリックしました';
 
 });
